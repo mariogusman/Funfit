02ea399402f164969f8085e1614f88aac079d12c
mariogusman <mariogusman@gmail.com>
2021-10-07 11:54:53 +0000

    updated webhook handler - fixed 400 error, and will now handle profiles

--- checkout/signals.py
+++ checkout/signals.py
@@ -15,5 +15,4 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    print("delete worked delete workeddelete workeddelete workeddelete workeddelete workeddelete worked")
     instance.order.update_total()
