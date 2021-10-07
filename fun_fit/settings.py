4669197739c19d40f614a1d0ce6ec0d71d74ce48
mariogusman <mariogusman@gmail.com>
2021-10-07 14:29:52 +0000

    1st step of deplyment loaded fixtures and connected to heroku

--- fun_fit/settings.py
+++ fun_fit/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,10 +127,10 @@ WSGI_APPLICATION = 'fun_fit.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 
--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -8,6 +9,8 @@ class ProductForm(forms.ModelForm):
         #defines the model and fields to be included
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         #override/change fields below using friendly names
